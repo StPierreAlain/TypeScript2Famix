5ebb7e7ecf20235f8a2b7cc1af89638751731847
Start2Run <mihaivs@yahoo.com>
2021-01-26 20:11:59 -0500

    Add sample files and ignore from loading them by the solution on build

--- src/sample.ts
+++ src/sample.ts
@@ -1,24 +1,14 @@
 import { Project } from "ts-morph";
-import {FamixBaseElement, FamixMseExporter, FamixRepository} from "famix"
-
 const project = new Project();
   
 try
 {
-    const sourceFiles = project.addSourceFilesAtPaths("E:/Scoala/MGL843/TP/TypescriptExample/src/*.ts");
-
-    const sourceFile = project.getSourceFileOrThrow("hello.ts");
-    
-    const hasClasses = sourceFile.getClasses().length > 0;
-    const interfaces = sourceFile.getInterfaces().length > 0;
-
-    // person interface
-    const personInterface = sourceFile.getInterface("Person")!;
-    personInterface.isDefaultExport(); // returns true
-    var name=personInterface.getName(); // returns "Person"
-    var properties=personInterface.getProperties(); // returns the properties
-    var prop=properties.length;
-    console.log(prop);
+    const sourceFiles = project.addSourceFilesAtPaths("C:/repos/TypeScript2Famix/resources/*.ts");
+    const sourceFile = project.getSourceFileOrThrow("Wall.ts");
+    //const hasClasses = sourceFile.getClasses().length > 0;
+    var namespaces = sourceFile.getNamespaces();
+    var namespaceName = namespaces[0].getName();
+    console.log(namespaces.length);
 }
 catch(Error)
 {
