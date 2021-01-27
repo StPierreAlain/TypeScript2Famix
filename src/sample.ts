06f91b5a35c2932782615a534603a2e82af3e5a0
Start2Run <mihaivs@yahoo.com>
2021-01-26 21:52:52 -0500

    Create MSE file containing class and namespace info

--- src/sample.ts
+++ src/sample.ts
@@ -1,14 +1,44 @@
 import { Project } from "ts-morph";
 const project = new Project();
-  
+
+import * as Famix from "./lib/model/famix";
+import * as FamixFile from "./lib/model/file";
+import {FamixBaseElement} from "./lib/famix_base_element";
+import {FamixMseExporter} from "./lib/famix_mse_exporter";
+import { FamixRepository } from './lib/famix_repository';
+
+import * as fs from 'fs';
+import * as path from 'path';
+
 try
 {
     const sourceFiles = project.addSourceFilesAtPaths("C:/repos/TypeScript2Famix/resources/*.ts");
-    const sourceFile = project.getSourceFileOrThrow("Wall.ts");
-    //const hasClasses = sourceFile.getClasses().length > 0;
-    var namespaces = sourceFile.getNamespaces();
-    var namespaceName = namespaces[0].getName();
-    console.log(namespaces.length);
+    
+    var fmxRep = new FamixRepository();
+
+    sourceFiles.forEach(file => {
+        var fmxFile = new FamixFile.File(fmxRep);
+        fmxFile.setName(file.getKindName());
+        fmxRep.addElement(fmxFile);
+
+        if (file.getNamespaces().length > 0) {
+            var namespace = file.getNamespaces()[0];
+            var fmxNamespace = new Famix.Namespace(fmxRep);
+            var name = namespace.getName();
+            fmxNamespace.setName(name);
+            fmxRep.addElement(fmxNamespace);
+            if (namespace.getClasses().length > 0) {
+                var cls = namespace.getClasses()[0];
+                var fmxClass = new Famix.Class(fmxRep);
+                var name = cls.getName();
+                fmxClass.setName(name);
+                fmxRep.addElement(fmxClass);
+             }
+         }
+    });
+    var mse = fmxRep.getMSE();
+    
+    fs.writeFile('sample.mse',mse, (err) => { if (err) throw err; });
 }
 catch(Error)
 {
