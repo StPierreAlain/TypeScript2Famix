09cc2d9acc59b2e402fae9a778acd79c97483efd
Start2Run <mihaivs@yahoo.com>
2021-01-27 11:46:56 -0500

    Add file anchor to the model

--- src/sample.ts
+++ src/sample.ts
@@ -8,31 +8,46 @@ import {FamixMseExporter} from "./lib/famix_mse_exporter";
 import { FamixRepository } from './lib/famix_repository';
 
 import * as fs from 'fs';
-import * as path from 'path';
 
 try
 {
-    const sourceFiles = project.addSourceFilesAtPaths("C:/repos/TypeScript2Famix/resources/*.ts");
+    const sourceFiles = project.addSourceFilesAtPaths("**/resources/*.ts");
     
     var fmxRep = new FamixRepository();
 
+    var fmxPackage = new Famix.Package(fmxRep);
+    let namespaces = new Map<string,Famix.Namespace>();
     sourceFiles.forEach(file => {
-        var fmxFile = new FamixFile.File(fmxRep);
-        fmxFile.setName(file.getKindName());
-        fmxRep.addElement(fmxFile);
+        var fmxFileAnchor = new Famix.FileAnchor(fmxRep);
+        fmxFileAnchor.setFileName(file.getBaseName())
+        fmxFileAnchor.setStartLine(file.getStartLineNumber())
+        fmxFileAnchor.setEndLine(file.getEndLineNumber())
 
         if (file.getNamespaces().length > 0) {
             var namespace = file.getNamespaces()[0];
-            var fmxNamespace = new Famix.Namespace(fmxRep);
             var name = namespace.getName();
-            fmxNamespace.setName(name);
-            fmxRep.addElement(fmxNamespace);
+
+            let fmxNamespace : Famix.Namespace;
+            if (namespaces.has(name)) { 
+                fmxNamespace = namespaces.get(name);
+            }
+            else
+            {
+                fmxNamespace = new Famix.Namespace(fmxRep);
+                fmxNamespace.setParentPackage(fmxPackage)    
+                fmxNamespace.setName(name);
+            }
+            
+            namespaces.set(name, fmxNamespace);
+           
             if (namespace.getClasses().length > 0) {
                 var cls = namespace.getClasses()[0];
                 var fmxClass = new Famix.Class(fmxRep);
-                var name = cls.getName();
-                fmxClass.setName(name);
+                var fileName = cls.getName();
+                fmxPackage.addChildNamedEntities(fmxClass);
+                fmxClass.setName(fileName);
                 fmxRep.addElement(fmxClass);
+                fmxClass.setParentPackage(fmxPackage)
              }
          }
     });
